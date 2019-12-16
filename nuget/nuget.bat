52e4bf3f06711092069f6c763b46f620c646287f
Dale Newman <dalenewman@gmail.com>
2019-12-16 16:12:06 -0500

    update dependencies

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -3,7 +3,7 @@ nuget pack Transformalize.Provider.Velocity.Autofac.nuspec -OutputDirectory "c:\
 nuget pack Transformalize.Transform.Velocity.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Transform.Velocity.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Provider.Velocity.0.6.3-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Provider.Velocity.Autofac.0.6.3-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Velocity.0.6.3-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Velocity.Autofac.0.6.3-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Velocity.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Provider.Velocity.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Velocity.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Velocity.Autofac.0.6.20-beta.nupkg" -source https://api.nuget.org/v3/index.json
